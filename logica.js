86deca84a0c1d0f8928e60e651114e5fa0523144
Andrea <andreamartinezdl1@gmail.com>
2026-02-05 21:22:27 -0600

    Funcionalidades ok

--- logica.js
+++ logica.js
@@ -1,1 +1,17 @@
+let celsius;//variable que cambiará
 
+while (true){
+    celsius= prompt("Ingresa tus grados Celsius")
+    //      no empty  and is a number
+    if (celsius !=="" && !isNaN(celsius)) {
+
+        celsius = Number(celsius)
+        break;
+    } else {
+        alert("¡Error! ingresa números únicamente")
+        
+    }
+}
+
+const fahrenheit = (celsius * 1.8.toFixed) + 32;
+const kelvin = celsius + 273.15.toFixed;
